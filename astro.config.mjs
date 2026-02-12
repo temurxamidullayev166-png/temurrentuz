593dd245fe26a18b4d4333d5690f51a13c2fa673
Temur <temurxamidullayev166@gmail.com>
2026-02-12 15:21:01 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,12 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
-// GitHub Pages config
 export default defineConfig({
   output: "static",
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  vite: { plugins: [tailwindcss()] },
   integrations: [react()],
 
-  // SHU YERNI O'ZINGGA MOSLAB YOZASAN:
-  // 1) USERNAME -> GitHub username
-  // 2) REPO -> repo nomi
+  // USERNAME va REPO ni o'zingnikiga yoz
   site: "https://USERNAME.github.io",
   base: "/REPO",
 });
